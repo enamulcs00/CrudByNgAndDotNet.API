aadec01c6fc14a7066241c64349c5ea950e925f1
Enamul Haque <enamulcs008@gmail.com>
2025-07-26 18:29:37 +0530

    Added RegisterUser and updated Identity configuration

--- CrudByNgAndDotNet.API/Migrations/AuthDb/20250726125814_UpdateToUseApplicationUser.Designer.cs
+++ CrudByNgAndDotNet.API/Migrations/AuthDb/20250726125814_UpdateToUseApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using CrudByNgAndDotNet.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CrudByNgAndDotNet.API.Migrations.AuthDb
 {
     [DbContext(typeof(AuthDbContext))]
-    partial class AuthDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250726125814_UpdateToUseApplicationUser")]
+    partial class UpdateToUseApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CrudByNgAndDotNet.API/Migrations/AuthDb/20250726125814_UpdateToUseApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudByNgAndDotNet.API.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class UpdateToUseApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "IdentityUser",
+                keyColumn: "Id",
+                keyValue: "edc267ec-d43c-4e3b-8108-a1a1f819906d",
+                columns: new[] { "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "db913dfc-eaf2-4633-99cf-2da11c8829c7", "ADMIN@CRUDBYNGANDDOTNET.COM", "ADMIN@CRUDBYNGANDDOTNET.COM", "AQAAAAIAAYagAAAAEBDy+9LLC8LPCfryjNIKRfWjp/IHL0dKzhLAnzVy9mmpG0YVWvf+ZJ/1EE2I2ctsQg==", "9a7c2b4d-9937-434b-85be-c574d7cdf63b" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "IdentityUser",
+                keyColumn: "Id",
+                keyValue: "edc267ec-d43c-4e3b-8108-a1a1f819906d",
+                columns: new[] { "ConcurrencyStamp", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "574a707e-61f2-4014-8dc4-019bb5495c60", "ADMIN@CrudByNgAndDotNet.COM", "ADMIN@CrudByNgAndDotNet.COM", "AQAAAAIAAYagAAAAELsu4th4WIJV47vTbarcgtHv4XWc7n1Y840hfLP3ImCSI9mEWBNn0fBZk+UpWM3iCQ==", "88b931e3-7281-447d-b3b2-4d2fb570e372" });
+        }
+    }
+}
